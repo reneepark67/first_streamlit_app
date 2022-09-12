86120a9d51f2d67f21e8c32a6ebc74afcb0252f5
Renee P <111900375+reneepark67@users.noreply.github.com>
2022-09-12 13:51:55 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text   ('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text  ('🐔 Hard-Boiled free-range eggs')
 streamlit.text  ('🥑🍞 Avocado Toast')
 
-
+#import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -25,11 +25,22 @@ streamlit.dataframe(fruits_to_show)
 
 #new Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+try:
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+if not fruit_choice:
+    streamlit.error ("Please select a fruit to get more information.")
+ else:
+  fruityvice_response = requests.get ("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
+  streamlist.dataframe (fruityvice_normalized)
+  
+ except URLError as e:
+    streamlist.error()
+
 streamlit.write('The user entered ', fruit_choice)
 
-
+#import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 
