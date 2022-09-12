d205d26b485309f39cd788a7561784574053fb31
Renee P <111900375+reneepark67@users.noreply.github.com>
2022-09-12 14:01:43 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ try:
      fruityvice_normalized = pandas.json_normalize (fruityvice_response.json())
      streamlist.dataframe (fruityvice_normalized)
   
- except URLError as e:
+except URLError as e:
     streamlist.error()
 
 streamlit.write('The user entered ', fruit_choice)
