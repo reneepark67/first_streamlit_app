d07682a488c77473046977f8629592579122af7c
Renee P <111900375+reneepark67@users.noreply.github.com>
2022-09-13 09:33:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥙 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 HArd-Boiled Free-Range Eggs')
+streamlit.text('🐔 Hard-Boiled Free-Range Eggs')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🍓 Build Your Own Fruit Smoothie 🥝🍇')
